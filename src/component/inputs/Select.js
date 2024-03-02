930b49500c3624c6f45770ddc640da7829dbfb56
YashVekariyaa <vekaryayash33@gmail.com>
2024-03-02 19:30:26 +0530

    Changes in select key

--- src/component/inputs/Select.js
+++ src/component/inputs/Select.js
@@ -7,9 +7,9 @@ const Select = (props) => {
             <div className="text-gray-400">{label}</div>
             <select className='block w-full py-2 pl-4 pr-4 text-black placeholder-gray-400 transition-all duration-200 border-2 border-gray-200 rounded-lg bg-white focus:outline-none focus:border-gray-400 focus:bg-white '  >
                 {
-                    data?.map((item) => {
+                    data?.map((item,index) => {
                         return (
-                            <option value={item?.value}>{item?.label}</option>
+                            <option key={index} value={item?.value}>{item?.label}</option>
                         )
                     })
                 }
