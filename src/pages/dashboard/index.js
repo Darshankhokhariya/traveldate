011391e5da43e0c39adddee1f31bf895255de344
YashVekariyaa <vekaryayash33@gmail.com>
2024-03-04 19:09:59 +0530

    make select inout

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,9 @@
 import Banner from "@/component/Dashboard/Banner/Banner";
 import Filter from "@/component/Dashboard/filters/Filter";
+import SelectTwo from "@/component/inputs/SelectTwo";
 import Recent from "@/component/recent/Recent";
 import Sidebar from "@/component/sidebar/Sidebar";
+import { gender } from "@/constant/gender";
 import React from "react";
 
 function dashboard() {
@@ -12,6 +14,7 @@ function dashboard() {
           <Banner />
           <Filter />
           <Recent />
+          <SelectTwo data={gender} label="Gender" />
         </div>
       </Sidebar>
     </>
